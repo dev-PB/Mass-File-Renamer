7e43fc8ddea76b8960eaa71fd624412c2b0548ea
Ryan W <47390291+onlinePB@users.noreply.github.com>
2022-02-28 04:10:57 +0000

    Created get_naming_convention() to allow user to input the naming convention

--- Renamer.py
+++ Renamer.py
@@ -14,10 +14,17 @@ def get_path():
     print("Selected path: " + path)
     return path
 
+def get_naming_convention():
+    print("Enter the naming convention: ")
+    naming_conv = input()
+    return naming_conv
+
+
 path = get_path()
 files = os.listdir(path)
+new_name_root = get_naming_convention()
 
 for i, file in enumerate(files):
     type = os.path.splitext(file)[1]
-    new_name = "test" + str(i) + type
+    new_name = new_name_root + str(i) + type
     os.rename(path + "\\" + file, path + "\\" + new_name)
